97060e5df9dfe6a2d165eabf02556cd4f7a9b40a
Game, Set and Stats <40372112+rajahn01@users.noreply.github.com>
2018-10-30 22:22:47 +0000

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,11 +1,11 @@
-getwd()
-setwd("OneDrive/Coursera/Data")
+#Read in the data
 data<-read.csv("household_power_consumption.txt", header=TRUE, sep=";")
 library(dplyr)
 
 # Create a new data frame which only includes the first two days of February 2007
 graphData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
 
+#Convert the variable to be plotted to a numerical value and plot it
 globalActivePower <- as.numeric(graphData$Global_active_power)
 png("plot1.png", width=480, height=480)
 hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
